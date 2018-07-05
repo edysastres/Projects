1c638166f4806c11fd57051700dfb4969ae70e53
rojovivo27 <rojovivo_27@hotmail.com>
2018-07-05 12:02:26 -0500

    Add files via upload

---
+++ ConsoleApp1 - sesion1/ConsoleApp1/Shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    //SESSION 1
+
+    class Shape
+    {
+        string name;
+        float area;
+        float perimeter;
+
+        public Shape()
+        {
+
+        }
+
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+
+        public string SayHello()
+        {
+
+            return "Hi! My name is " + name;
+
+        }
+
+        protected void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        protected void SetArea(float area)
+        {
+            this.area = area;
+        }
+
+        protected void SetPerimeter(float perimeter)
+        {
+            this.perimeter = perimeter;
+        }
+
+        public float GetArea()
+        {
+            return area;
+        }
+    }
+
+
+}
---
+++ ConsoleApp1 - sesion1/ConsoleApp1/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    //SESSION 2
+
+    class Square : Shape
+    {
+        float side;
+
+        public Square()
+        {
+
+        }
+
+        public Square(string name)
+        {
+            base.SetName(name);
+        }
+        public Square(float side)
+        {
+            this.side = side;
+            float area = side * side;
+            base.SetArea(area);
+            float perimeter = side * 4;
+            base.SetPerimeter(perimeter);
+        }
+
+        public Square(string name, float side)
+        {
+            base.SetName(name);
+            this.side = side;
+            float area = side * side;
+            base.SetArea(area);
+            float perimeter = side * 4;
+            base.SetPerimeter(perimeter);
+        }
+    }
+}
