2a0bfd2a59eb31327b90453cd7e2e9ee1291fb53
Daniel Flores <soydanflr@gmail.com>
2018-07-18 09:49:46 -0500

    Agregado proyecto de conexión con el checador
    Se creó una nueva carpeta para almacenar los proyectos de conexión con otros dispositivos que se realicen en la empresa, y se agregó en ella el proyecto utilizado para establecer una conexión con el lector de manos

---
+++ Interfaces de conexión/ConexionChecador/TestChecador/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace TestChecador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblIPAddr = new System.Windows.Forms.Label();
+            this.btnChangeAddr = new System.Windows.Forms.Button();
+            this.btnBeep = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dgRegistros = new System.Windows.Forms.DataGridView();
+            this.Empleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnRefresh);
+            this.panel1.Controls.Add(this.btnConnect);
+            this.panel1.Controls.Add(this.lblIPAddr);
+            this.panel1.Controls.Add(this.btnChangeAddr);
+            this.panel1.Controls.Add(this.btnBeep);
+            this.panel1.Controls.Add(this.lblStatus);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(643, 126);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(509, 97);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(121, 23);
+            this.btnRefresh.TabIndex = 11;
+            this.btnRefresh.Text = "Actualizar registros";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(509, 14);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 23);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Conectar";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblIPAddr
+            // 
+            this.lblIPAddr.AutoSize = true;
+            this.lblIPAddr.Location = new System.Drawing.Point(192, 72);
+            this.lblIPAddr.Name = "lblIPAddr";
+            this.lblIPAddr.Size = new System.Drawing.Size(0, 13);
+            this.lblIPAddr.TabIndex = 9;
+            // 
+            // btnChangeAddr
+            // 
+            this.btnChangeAddr.Location = new System.Drawing.Point(509, 68);
+            this.btnChangeAddr.Name = "btnChangeAddr";
+            this.btnChangeAddr.Size = new System.Drawing.Size(121, 23);
+            this.btnChangeAddr.TabIndex = 8;
+            this.btnChangeAddr.Text = "Cambiar IP/Reloj";
+            this.btnChangeAddr.UseVisualStyleBackColor = true;
+            // 
+            // btnBeep
+            // 
+            this.btnBeep.Location = new System.Drawing.Point(509, 42);
+            this.btnBeep.Name = "btnBeep";
+            this.btnBeep.Size = new System.Drawing.Size(121, 23);
+            this.btnBeep.TabIndex = 7;
+            this.btnBeep.Text = "Emitir sonido";
+            this.btnBeep.UseVisualStyleBackColor = true;
+            this.btnBeep.Click += new System.EventHandler(this.btnBeep_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(326, 72);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(10, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Estado:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dirección IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "HandPunch 2000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Control de asistencias";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dgRegistros
+            // 
+            this.dgRegistros.AllowUserToAddRows = false;
+            this.dgRegistros.AllowUserToDeleteRows = false;
+            this.dgRegistros.AllowUserToResizeRows = false;
+            this.dgRegistros.BackgroundColor = System.Drawing.Color.White;
+            this.dgRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgRegistros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Empleado,
+            this.Fecha,
+            this.Hora});
+            this.dgRegistros.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgRegistros.Location = new System.Drawing.Point(0, 126);
+            this.dgRegistros.Name = "dgRegistros";
+            this.dgRegistros.Size = new System.Drawing.Size(643, 372);
+            this.dgRegistros.TabIndex = 1;
+            // 
+            // Empleado
+            // 
+            this.Empleado.HeaderText = "No. Empleado";
+            this.Empleado.Name = "Empleado";
+            this.Empleado.Width = 200;
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha Registro";
+            this.Fecha.Name = "Fecha";
+            this.Fecha.Width = 200;
+            // 
+            // Hora
+            // 
+            this.Hora.HeaderText = "Hora Registro";
+            this.Hora.Name = "Hora";
+            this.Hora.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 498);
+            this.Controls.Add(this.dgRegistros);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Control de asistencias";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgRegistros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnBeep;
+        private System.Windows.Forms.Button btnChangeAddr;
+        private System.Windows.Forms.DataGridView dgRegistros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Empleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hora;
+        private System.Windows.Forms.Label lblIPAddr;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ Interfaces de conexión/ConexionChecador/TestChecador/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RecogSys.RdrAccess;
+using System.IO;
+
+namespace TestChecador
+{
+    public partial class Form1 : Form
+    {
+
+        private CRsiNetwork Red = new CRsiNetwork();
+        private CRsiHandReader Device = new CRsiHandReader();
+        private CRsiComWinsock Com = new CRsiComWinsock();
+        private bool connectionState = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Conectar()
+        {
+            try
+            {
+                Com.SetPortA(3001);
+                Com.SetHost(Properties.Resources.IPAddress);
+                Red.SetCom(Com);
+                if (Com.Connect() == 0)
+                {
+                    Com.Disconnect();
+                    Com.ResetSocket();
+                    Com.Connect();
+                }
+                Red.SetNetworkName("TCP/IP");
+                if (Red.IsConnected() == 0)
+                {
+                    throw new Exception("No existe una conexión con el dispositivo");
+                }
+                Red.Attach(Device);
+                Device.SetAddress(0);
+                Device.SetName("HandPunch 2000");
+                Device.CmdBeep(100, 3);
+                if (Device.IsConnected() == 0)
+                {
+                    Desconectar();
+                    throw new Exception("Dispositivo no conectado");
+                } else{
+                    btnBeep.Enabled = true;
+                    btnRefresh.Enabled = true;
+                    lblIPAddr.Text = Properties.Resources.IPAddress;
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Text = "Conectado";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error en la conexión", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly, false);
+            }
+        }
+
+        private void Desconectar()
+        {
+            Com.Disconnect();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!connectionState)
+            {
+                Conectar();
+                btnConnect.Text = "Desconectar";
+            }
+            else
+            {
+                Desconectar();
+                btnBeep.Enabled = false;
+                btnRefresh.Enabled = false;
+                lblStatus.Text = "Desconectado";
+                lblStatus.ForeColor = Color.Red;
+                btnConnect.Text = "Conectar";
+            }
+            connectionState = !connectionState;
+            RSI_DATALOG data = new RSI_DATALOG();
+            RSI_ID employeeID = new RSI_ID();
+            while (Device.CmdGetDatalog(data) > 0)
+            {
+                    int year = int.Parse(data.pTimestamp.year.ToString()) + 2000;
+                    int month = int.Parse(data.pTimestamp.month.ToString());
+                    int day = int.Parse(data.pTimestamp.day.ToString());
+                    int hour = int.Parse(data.pTimestamp.hour.ToString());
+                    int minute = int.Parse(data.pTimestamp.minute.ToString());
+                    int seconds = int.Parse(data.pTimestamp.second.ToString());
+                    if (year == 2000) break;
+                    DateTime date = new DateTime(year, month, day, hour, minute, seconds);
+                    if (data.format == RSI_DATALOG_FORMAT.RSI_DLF_IDENTITY_VERIFIED) dgRegistros.Rows.Add(data.pOperand.GetID().Substring(data.pOperand.GetID().Length - 3), date.Date.ToShortDateString(), date.TimeOfDay);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!connectionState)
+            {
+                Conectar();
+                btnConnect.Text = "Desconectar";
+            }
+            else
+            {
+                Desconectar();
+                btnBeep.Enabled = false;
+                btnRefresh.Enabled = false;
+                lblStatus.Text = "Desconectado";
+                lblStatus.ForeColor = Color.Red;
+                btnConnect.Text = "Conectar";
+            }
+            connectionState = !connectionState;
+        }
+
+        private void btnBeep_Click(object sender, EventArgs e)
+        {
+            Device.CmdBeep(100, 1);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //System.Environment.Exit(0);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RSI_DATALOG data = new RSI_DATALOG();
+            RSI_ID employeeID = new RSI_ID();
+            while (Device.CmdGetDatalog(data) > 0)
+            {
+                int year = int.Parse(data.pTimestamp.year.ToString()) + 2000;
+                int month = int.Parse(data.pTimestamp.month.ToString());
+                int day = int.Parse(data.pTimestamp.day.ToString());
+                int hour = int.Parse(data.pTimestamp.hour.ToString());
+                int minute = int.Parse(data.pTimestamp.minute.ToString());
+                int seconds = int.Parse(data.pTimestamp.second.ToString());
+                if (year == 2000) break;
+                DateTime date = new DateTime(year, month, day, hour, minute, seconds);
+                if (data.format == RSI_DATALOG_FORMAT.RSI_DLF_IDENTITY_VERIFIED) dgRegistros.Rows.Add(data.pOperand.GetID().Substring(data.pOperand.GetID().Length - 3), date.Date.ToShortDateString(), date.TimeOfDay);
+                RSI_DISPLAY_MESSAGE Msg = new RSI_DISPLAY_MESSAGE();
+                Msg.pMsg.Set("Registros descargados");
+                Device.CmdPutDisplayMessage(Msg);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Red != null)
+            {
+                Red.SetCom(null);
+                Red.Dispose();
+                Red = null;
+            }
+            if (Com != null)
+            {
+                Com.Dispose();
+                Com = null;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                RSI_DATALOG data = new RSI_DATALOG();
+                RSI_ID employeeID = new RSI_ID();
+                while (Device.CmdGetDatalog(data) > 0)
+                {
+                    int year = int.Parse(data.pTimestamp.year.ToString()) + 2000;
+                    int month = int.Parse(data.pTimestamp.month.ToString());
+                    int day = int.Parse(data.pTimestamp.day.ToString());
+                    int hour = int.Parse(data.pTimestamp.hour.ToString());
+                    int minute = int.Parse(data.pTimestamp.minute.ToString());
+                    int seconds = int.Parse(data.pTimestamp.second.ToString());
+                    if (year == 2000) break;
+                    DateTime date = new DateTime(year, month, day, hour, minute, seconds);
+                    if (data.format == RSI_DATALOG_FORMAT.RSI_DLF_IDENTITY_VERIFIED) dgRegistros.Rows.Add(data.pOperand.GetID().Substring(data.pOperand.GetID().Length - 3), date.Date.ToShortDateString(), date.TimeOfDay);
+                    RSI_DISPLAY_MESSAGE Msg = new RSI_DISPLAY_MESSAGE();
+                    Msg.pMsg.Set("Registros descargados");
+                    Device.CmdPutDisplayMessage(Msg);
+                }
+            }
+        }
+    }
+}
